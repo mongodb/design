f3692f5356642002bae473527cab48e94816d2ef
Melissa Wong <melissa.wong89@gmail.com>
2016-10-21 11:12:53 -0400

    Added mobile media queries

--- client/components/brand-design-system/index.jsx
+++ client/components/brand-design-system/index.jsx
@@ -8,16 +8,17 @@ import '../../styling/root.less';
 
 const BrandDesignSystem = ({ children }) => (
   <div className="wrap">
-    <nav className="sidebar">
-    	<ul>
-    		<li><Link to='/brand-design-system/'>Welcome</Link></li>
-            <li><Link to='/brand-design-system/logo'>Logo</Link></li>
-    		<li><Link to='/brand-design-system/typography'>Typography</Link></li>
-    		<li><Link to='/brand-design-system/colors'>Colors</Link></li>
-    	</ul>
-    </nav>
-    <div className="content">{children}</div>
+        <nav className="sidebar">
+           	<ul>
+        		<li><Link to='/brand-design-system/'>Welcome</Link></li>
+                <li><Link to='/brand-design-system/logo'>Logo</Link></li>
+        		<li><Link to='/brand-design-system/typography'>Typography</Link></li>
+        		<li><Link to='/brand-design-system/colors'>Colors</Link></li>
+        	</ul>
+        </nav>
+    <div className="content #page-content-wrapper">{children}</div>
   </div>
 );
 
 export default BrandDesignSystem;
+
--- client/components/brand-design-system/logo/index.jsx
+++ client/components/brand-design-system/logo/index.jsx
@@ -9,26 +9,31 @@ var logoLockup = require('../../../assets/logo-lockup.png');
 const BrandLogo = () => (
     <div className="wrap">
         <h1 className="heading">Logo</h1>
-        <div className="row">
-            <div className="col-md-6">
-                <h3>Basic Usage Guidelines</h3>
-                <ul>
-                    <li>Do not rotate the MongoDB logo.</li>
-                    <li>Do not change the color of the MongoDB logo.</li>
-                    <li>Do not distort the MongoDB logo - be sure to scale proportionately.</li>
-                    <li>Allow an appropriate amount of white space around the MongoDB logo, at least as much as the x-height of the logo (suggested).</li>
-                    <li>Preferred minimum size for the MongoDB logo is 100px wide. For smaller sizes, you must seek permission from the MongoDB marketing team.</li>
-                </ul>
+        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="content">
+                    <h3>Basic Usage Guidelines</h3>
+                    <ul>
+                        <li>Do not rotate the MongoDB logo.</li>
+                        <li>Do not change the color of the MongoDB logo.</li>
+                        <li>Do not distort the MongoDB logo - be sure to scale proportionately.</li>
+                        <li>Allow an appropriate amount of white space around the MongoDB logo, at least as much as the x-height of the logo (suggested).</li>
+                        <li>Preferred minimum size for the MongoDB logo is 100px wide. For smaller sizes, you must seek permission from the MongoDB marketing team.</li>
+                    </ul>
+                </div>
+                <div className="content">
+                    <img alt="MongoDB logo" src="https://webassets.mongodb.com/_com_assets/cms/mongodb-logo-rgb-j6w271g1xn.jpg"></img>
+                </div>
             </div>
-            <div className="col-md-6">
-                <img className="img-responsive" style={{ maxWidth:'300' }} alt="mongodb logo" src="https://webassets.mongodb.com/_com_assets/cms/mongodb-logo-rgb-j6w271g1xn.jpg"></img>
-            </div>
-        </div>
-        <hr/>
-        <div className="row">
-            <div className="col-md-6">
-                <p>Insert images of logo lockups</p>
-                <img src={logoLockup}/>
+            <hr/>
+            <div className="row">
+                <div className="content">
+                    <h3>Logo | Structure</h3>
+                        <div className="content">
+                            <img alt="MongoDB logo lock" src={logoLockup}/>
+                        </div>
+                </div>
             </div>
         </div>
     </div>
