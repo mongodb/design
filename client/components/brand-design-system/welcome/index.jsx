d8cf39ccc617230537a4ee3c3685f70de7158455
Melissa Wong <melissa.wong89@gmail.com>
2016-10-25 14:17:55 -0400

    Added brand guide link

--- client/components/brand-design-system/welcome/index.jsx
+++ client/components/brand-design-system/welcome/index.jsx
@@ -12,7 +12,7 @@ const BrandWelcome = () => (
                 <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
                 <p>We also have a brand guide you should check out.</p>
             </div>
-            <form method="get" action="file.doc">
+            <form method="get" action="https://webassets.mongodb.com/MongoDB_Brand_Resources.zip">
                 <button className="button button-is-info" type="submit">Download the Brand Guide</button>
             </form>
     </div>
