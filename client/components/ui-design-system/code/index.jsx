19086c1204cd344d6d3d22b1b52079b6c5e5650a
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-07-20 13:50:56 -0400

    replaced with code from design toolkit

--- client/components/ui-design-system/code/index.jsx
+++ client/components/ui-design-system/code/index.jsx
@@ -17,17 +17,19 @@ const UICode = () => (
         <h2>Code Styles</h2>
       </div>
     </div>
-
     <div className="row u-mb-2">
       <div className="columns small-12">
         <h3>Copyable Command</h3>
       </div>
     </div>
+
     <div className="row u-mb-2">
       <div className="columns small-12">
-        <div className="snippet">
-          <div className="snippet-is-single">this is a command that you can copy. it won't break onto the next line even if it's really long, the overflow will keep scrolling horizontally.</div>
-          <button className="snippet-has-button">copy</button>
+        <div className="copy-command">
+          <span className="copy-command-text">this is a command that you can copy. it will not break onto the next line even if it is really long, the overflow will keep scrolling horizontally.</span>
+          <button className="copy-command-button">
+            <i className="fa fa-files-o"></i> Copy
+          </button>
         </div>
       </div>
     </div>
@@ -35,9 +37,61 @@ const UICode = () => (
       <div className="columns small-12">
         <pre>
           <code className="language-html hljs xml" data-lang="html">
-            <div>{ '<div className="snippet">' }</div>
-            <div>{ '    <div className="snippet-is-single">this is a command that you can copy. it wont break onto the next line even if its really long, the overflow will keep scrolling horizontally.</div>' }</div>
-            <div>{ '    <button className="snippet-has-button">copy</button>' }</div>
+            <div>{ '<div className="copy-command">' }</div>
+            <div>{ '  <span className="copy-command-text">this is a command that you can copy. it will break onto the next line even if it is really long, the overflow will keep scrolling horizontally.</span>' }</div>
+            <div>{ '  <button className="copy-command-button">' }</div>
+            <div>{ '    <i className="fa fa-files-o"></i> Copy' }</div>
+            <div>{ '  </button>' }</div>
+            <div>{ '</div>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row u-mb-2">
+      <div className="columns small-12">
+        <div className="copy-command">
+          <span className="copy-command-text">by default these are not full width</span>
+          <button className="copy-command-button">
+            <i className="fa fa-files-o"></i> Copy
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<div className="copy-command">' }</div>
+            <div>{ '  <span className="copy-command-text">by default these are not full width</span>' }</div>
+            <div>{ '  <button className="copy-command-button">' }</div>
+            <div>{ '    <i className="fa fa-files-o"></i> Copy' }</div>
+            <div>{ '  </button>' }</div>
+            <div>{ '</div>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row u-mb-2">
+      <div className="columns small-12">
+        <div className="copy-command copy-command-is-full-width">
+          <span className="copy-command-text copy-command-text-is-full-width">by default these are not full width</span>
+          <button className="copy-command-button">
+            <i className="fa fa-files-o"></i> Copy
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<div className="copy-command copy-command-is-full-width">' }</div>
+            <div>{ '  <span className="copy-command-text copy-command-text-is-full-width">but you can force them to be</span>' }</div>
+            <div>{ '  <button className="copy-command-button">' }</div>
+            <div>{ '    <i className="fa fa-files-o"></i> Copy' }</div>
+            <div>{ '  </button>' }</div>
             <div>{ '</div>' }</div>
           </code>
         </pre>
@@ -49,27 +103,17 @@ const UICode = () => (
         <h3>Codeblock</h3>
       </div>
     </div>
+
     <div className="row u-mb-2">
       <div className="columns small-12">
-        <div className="snippet">
-            <div className="snippet-has-multiple">this is a code block.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    
-            </div>
-        </div>
+        <div className="codeblock">blah</div>
       </div>
     </div>
     <div className="row u-mb-3">
       <div className="columns small-12">
         <pre>
           <code className="language-html hljs xml" data-lang="html">
-            <div>{ '<div className="snippet">' }</div>
-            <div>{ '    <div className="snippet-has-multiple"> const exampleCodeblock = {' }</div>
-            <div>{ '        description: this is a codeblock that preserves whitespace' }</div>
-            <div>{ '        overflow: this is also scrollable horizontally, so it wont break onto the next line even if its really long, it just keeps scrolling and scrolling and scrolling...' } </div>
-            <div>{ '    };</div>' }</div>
+            <div>{ '<div className="codeblock">' }</div>
             <div>{ '</div>' }</div>
           </code>
         </pre>
