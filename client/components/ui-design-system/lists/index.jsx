319afe2cf1bcf83f5c87b0c0c40be467f3d58b5a
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-07-21 14:12:37 -0400

    added list directory and styles for checklist, nested list, number circle, step indicator, pagination controls

---
+++ client/components/ui-design-system/lists/index.jsx
@@ -0,0 +1,244 @@
+// ==================================================
+//  DesignSystem - DSTables
+// ==================================================
+
+import React from 'react';
+import '../../../styling/root.less';
+
+const UILists = () => (
+  <div className="wrap">
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <h1>Lists</h1>
+      </div>
+    </div>
+    <div className="row">
+      <div className="columns small-12">
+        <h3>Checklist</h3>
+        <ul className="checklist">
+          <li className="checklist-item">This is the first feature you get</li>
+          <li className="checklist-item">You probably like this feature even more</li>
+          <li className="checklist-item">Wow! And there's one more. You gotta buy this</li>
+        </ul>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<ul className="checklist">' }</div>
+            <div>{ '  <li className="checklist-item">This is the first feature you get</li>' }</div>
+            <div>{ '  <li className="checklist-item">You probably like this feature even more</li>' }</div>
+            <div>{ '  <li className="checklist-item">Wow! And there\'s one more. You gotta buy this</li>' }</div>
+            <div>{ '</ul>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="columns small-12">
+        <h3>Nested List</h3>
+        <ul className="nested-list">
+          <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> books</a>
+            <ul className="nested-list-child-container">
+              <li><a className="nested-list-child-link" href="">authors</a></li>
+              <li><a className="nested-list-child-link" href="">agents</a></li>
+              <li><a className="nested-list-child-link" href="">publishers</a></li>
+              <li><a className="nested-list-child-link" href="">bookstores</a></li>
+            </ul>
+          </li>
+          <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> cinema</a>
+            <ul className="nested-list-child-container">
+              <li><a className="nested-list-child-link" href="">films</a></li>
+              <li><a className="nested-list-child-link" href="">actors</a></li>
+              <li><a className="nested-list-child-link" href="">directors</a></li>
+              <li><a className="nested-list-child-link" href="">writers</a></li>
+              <li><a className="nested-list-child-link" href="">agents</a></li>
+              <li><a className="nested-list-child-link" href="">studios</a></li>
+              <li><a className="nested-list-child-link" href="">theaters</a></li>
+            </ul>
+          </li>
+          <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> music</a>
+            <ul className="nested-list-child-container">
+              <li><a className="nested-list-child-link" href="">bands</a></li>
+              <li><a className="nested-list-child-link" href="">singers</a></li>
+              <li><a className="nested-list-child-link" href="">songwriters</a></li>
+              <li><a className="nested-list-child-link" href="">managers</a></li>
+              <li><a className="nested-list-child-link" href="">studios</a></li>
+              <li><a className="nested-list-child-link" href="">venues</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<ul className="nested-list">' }</div>
+            <div>{ '  <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> books</a>' }</div>
+            <div>{ '    <ul className="nested-list-child-container">' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">authors</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">agents</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">publishers</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">bookstores</a></li>' }</div>
+            <div>{ '    </ul>' }</div>
+            <div>{ '  </li>' }</div>
+            <div>{ '  <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> cinema</a>' }</div>
+            <div>{ '    <ul className="nested-list-child-container">' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">films</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">actors</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">directors</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">writers</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">agents</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">studios</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">theaters</a></li>' }</div>
+            <div>{ '    </ul>' }</div>
+            <div>{ '  </li>' }</div>
+            <div>{ '  <li><a className="nested-list-parent-link" href=""><i className="mms-icon-database nested-list-icon"></i> music</a>' }</div>
+            <div>{ '    <ul className="nested-list-child-container">' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">bands</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">singers</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">songwriters</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">managers</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">studios</a></li>' }</div>
+            <div>{ '      <li><a className="nested-list-child-link" href="">venues</a></li>' }</div>
+            <div>{ '    </ul>' }</div>
+            <div>{ '  </li>' }</div>
+            <div>{ '</ul>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="columns small-12">
+        <h3>Number Circles</h3>
+        <p>For showing wizard progress.</p>
+        <span className="number-circle u-mr-2">1</span>
+        <span className="number-circle number-circle-is-current u-mr-2">2</span>
+        <span className="number-circle number-circle-is-complete u-mr-2">✓</span>
+        <span className="number-circle number-circle-is-pending u-mr-2">4</span>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<span className="number-circle u-mr-2">1</span>' }</div>
+            <div>{ '<span className="number-circle number-circle-is-current u-mr-2">2</span>' }</div>
+            <div>{ '<span className="number-circle number-circle-is-complete u-mr-2">✓</span>' }</div>
+            <div>{ '<span className="number-circle number-circle-is-pending u-mr-2">4</span>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="columns small-12">
+        <h3>Step Indicator</h3>
+        <ol className="step-indicator">
+          <li className="step-indicator-step step-indicator-step-is-complete">Rey, these are your first steps</li>
+          <li className="step-indicator-step">This is your second step</li>
+          <li className="step-indicator-step step-indicator-step-is-inactive">This is your third step. Keep them short.</li>
+        </ol>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<ol className="step-indicator">' }</div>
+            <div>{ '  <li className="step-indicator-step step-indicator-step-is-complete">Rey, these are your first steps</li>' }</div>
+            <div>{ '  <li className="step-indicator-step">This is your second step</li>' }</div>
+            <div>{ '  <li className="step-indicator-step step-indicator-step-is-inactive">This is your third step. Keep them short.</li>' }</div>
+            <div>{ '</ol>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="columns small-12">
+        <h3>Pagination Controls</h3>
+        <div className="paginator">
+          <span className="paginator-control paginator-control-is-left">
+            <button className="paginator-button paginator-button-is-previous">
+              <i className="fa fa-angle-left"></i>
+            </button> Previous
+          </span>
+          <span className="paginator-text">
+            <strong>100-200 results (both controls enabled)</strong>
+          </span>
+          <span className="paginator-control paginator-control-is-right">Next
+            <button className="paginator-button paginator-button-is-next">
+              <i className="fa fa-angle-right"></i>
+            </button>
+          </span>
+        </div>
+        <div className="paginator paginator-is-borderless">
+          <span className="paginator-control paginator-control-is-left paginator-control-is-disabled">
+            <button className="paginator-button paginator-button-is-previous" disabled="">
+              <i className="fa fa-angle-left"></i>
+            </button> Previous
+          </span>
+          <span className="paginator-text">
+            <strong>0-0 results (both controls disabled, borderless example)</strong>
+          </span>
+          <span className="paginator-control paginator-control-is-right paginator-control-is-disabled">Next
+            <button className="paginator-button paginator-button-is-next" disabled="">
+              <i className="fa fa-angle-right"></i>
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <pre>
+          <code className="language-html hljs xml" data-lang="html">
+            <div>{ '<div className="paginator">' }</div>
+            <div>{ '  <span className="paginator-control paginator-control-is-left">' }</div>
+            <div>{ '    <button className="paginator-button paginator-button-is-previous">' }</div>
+            <div>{ '      <i className="fa fa-angle-left"></i>' }</div>
+            <div>{ '    </button> Previous' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '  <span className="paginator-text">' }</div>
+            <div>{ '    <strong>100-200 results (both controls enabled)</strong>' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '  <span className="paginator-control paginator-control-is-right">Next' }</div>
+            <div>{ '    <button className="paginator-button paginator-button-is-next">' }</div>
+            <div>{ '      <i className="fa fa-angle-right"></i>' }</div>
+            <div>{ '    </button>' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '</div>' }</div>
+            <div>{ '<div className="paginator paginator-is-borderless">' }</div>
+            <div>{ '  <span className="paginator-control paginator-control-is-left paginator-control-is-disabled">' }</div>
+            <div>{ '    <button className="paginator-button paginator-button-is-previous" disabled="">' }</div>
+            <div>{ '      <i className="fa fa-angle-left"></i>' }</div>
+            <div>{ '    </button> Previous' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '  <span className="paginator-text">' }</div>
+            <div>{ '    <strong>0-0 results (both controls disabled, borderless example)</strong>' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '  <span className="paginator-control paginator-control-is-right paginator-control-is-disabled">Next' }</div>
+            <div>{ '    <button className="paginator-button paginator-button-is-next" disabled="">' }</div>
+            <div>{ '      <i className="fa fa-angle-right"></i>' }</div>
+            <div>{ '    </button>' }</div>
+            <div>{ '  </span>' }</div>
+            <div>{ '</div>' }</div>
+          </code>
+        </pre>
+      </div>
+    </div>
+
+
+
+
+
+
+  </div>
+);
+
+export default UILists;
