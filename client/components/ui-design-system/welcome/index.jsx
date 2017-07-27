017e771a874b38b453c388bfc5af500a5ad050f6
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-07-27 09:07:22 -0400

    added hero and value props

--- client/components/ui-design-system/welcome/index.jsx
+++ client/components/ui-design-system/welcome/index.jsx
@@ -7,8 +7,54 @@ import '../../../styling/root.less';
 
 const UIWelcome = () => (
   <div className="wrap">
-    <h1 className="heading">Welcome to MongoDB&apos;s UI Kit</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eleifend ipsum sit amet lectus rutrum pulvinar. Praesent efficitur leo sed felis tincidunt, sed feugiat quam hendrerit. Vivamus et sagittis neque, nec malesuada odio. Proin scelerisque in nulla non porttitor. Sed convallis et sapien non sodales.</p>
+  	<div className="row u-mb-9">
+    	<div className="columns small-5">
+    		<div className="hero">
+    			<div className="hero-container">
+				  	<h2 className="home-subheader">A sub-headline suggesting something</h2>
+				  	<h1 className="home-header">Some copy welcoming someone to the site.</h1>
+				  	<a className="link home-link">Explore the Design System</a>
+			  	</div>
+		  	</div>
+	  	</div>
+  	</div>
+
+  	<div className="row u-mb-9">
+	  	<div className="columns small-7">
+		  	<img className="home-img"></img>
+	  	</div>
+    	<div className="columns small-5">
+		  	<h2 className="home-subheader">Value #1</h2>
+		  	<h1 className="home-sub-subheader">Something we value as a design team.</h1>
+		  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in finibus nisl. Cras lectus purus, placerat sit amet urna interdum, vestibulum facilisis quam.</p>
+	  	</div>
+  	</div>
+
+  	<div className="row u-mb-9">
+    	<div className="columns small-5">
+		  	<h2 className="home-subheader">Value #2</h2>
+		  	<h1 className="home-sub-subheader">Something we value as a design team.</h1>
+		  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in finibus nisl. Cras lectus purus, placerat sit amet urna interdum, vestibulum facilisis quam.</p>
+	  	</div>
+	  	<div className="columns small-7">
+		  	<img className="home-img home-img-right"></img>
+		</div>
+  	</div>
+
+  	<div className="row u-mb-9">
+	  	<div className="columns small-7">
+		  	<img className="home-img"></img>
+	  	</div>
+    	<div className="columns small-5">
+		  	<h2 className="home-subheader">Value #3</h2>
+		  	<h1 className="home-sub-subheader">Something we value as a design team.</h1>
+		  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in finibus nisl. Cras lectus purus, placerat sit amet urna interdum, vestibulum facilisis quam.</p>
+	  	</div>
+  	</div>
+
+
+
+
   </div>
 );
 
