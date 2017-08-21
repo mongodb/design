0cd4b72d19a144f595a6ec0de351bcc3a15233e1
Joy Sampoonachot <Joy.sampoonachot@gmail.com>
2017-08-21 10:21:24 -0400

    js-feat-home (#18)

--- client/components/brand-design-system/welcome/index.jsx
+++ client/components/brand-design-system/welcome/index.jsx
@@ -10,7 +10,7 @@ const BrandWelcome = () => (
         <div className="columns small-12">
             <h1 className="heading">Welcome (Brand)</h1>
         </div>
-        <div className="columns small-12">
+        <div className="columns small-12 u-mb-3">
             <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
             <p>We also have a brand guide you should check out.</p>
         </div>
