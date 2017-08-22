477e2da43e7c85dd9135de59d79a52ea091d6aa8
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-08-22 13:29:13 -0400

    reverted the 404 page

--- client/components/not_found.jsx
+++ client/components/not_found.jsx
@@ -3,8 +3,11 @@
 // ==================================================
 
 import React from 'react';
-import UIDesignSystem from './ui-design-system/index.jsx';
+// import UIDesignSystem from './ui-design-system/index.jsx';
+// const NotFound = () => UIDesignSystem({ children: (<div>404</div>)});
 
-const NotFound = () => UIDesignSystem({ children: (<div>404</div>)});
+const NotFound = () => (
+	<div>404</div>
+);
 
 export default NotFound;
