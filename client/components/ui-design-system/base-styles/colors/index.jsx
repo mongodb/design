f2e4379e7f7608f7b14313c2c661f07a1479fbb8
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-09-20 15:52:30 -0400

    styled the code

--- client/components/ui-design-system/base-styles/colors/index.jsx
+++ client/components/ui-design-system/base-styles/colors/index.jsx
@@ -19,46 +19,41 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block green0">
-          <span className="code">@green0</span>
-        </span>
+        <span className="color-block green0"></span>
+        <span className="code code-left">@green0</span>
+        <span className="code code-hex">#365221</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block green1">
-          <span className="code">@green1</span>
-        </span>
+        <span className="color-block green1"></span>
+        <span className="code code-left">@green1</span>
+        <span className="code code-hex">#507b32</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block green2">
-          <span className="code">@green2</span>
-        </span>
+        <span className="color-block green2"></span>
+        <span className="code code-left">@green2</span>
+        <span className="code code-hex">#6ba442</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block green3">
-          <span className="code">@green3</span>
-        </span>
+        <span className="color-block green3"></span>
+        <span className="code code-left">@green3</span>
+        <span className="code code-hex">#89b668</span>
       </div>
     </div>
     <div className="row u-mb-5">
       <div className="columns small-3">
-        <span className="color-block green4">
-          <span className="code">@green4</span>
-        </span>
+        <span className="color-block green4"></span>
+        <span className="code code-left">@green4</span>
+        <span className="code code-hex">#a6c88e</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block green5">
-          <span className="code">@green5</span>
-        </span>
+        <span className="color-block green5"></span>
+        <span className="code code-left">@green5</span>
+        <span className="code code-hex">#c4dbb3</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block green8">
-          <span className="code">@green8</span>
-        </span>
-      </div>
-      <div className="columns small-3">
-        <span className="color-block mongodbgreen">
-          <span className="code">@mongodbgreen</span>
-        </span>
+        <span className="color-block green8"></span>
+        <span className="code code-left">@green8</span>
+        <span className="code code-hex">#f0f7ec</span>
       </div>
     </div>
 
@@ -69,58 +64,53 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block gray0">
-          <span className="code">@gray0</span>
-        </span>
+        <span className="color-block gray0"></span>
+        <span className="code code-left">@gray0</span>
+        <span className="code code-hex">#303434</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray1">
-          <span className="code">@gray1</span>
-        </span>
+        <span className="color-block gray1"></span>
+        <span className="code code-left">@gray1</span>
+        <span className="code code-hex">#464c4f</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray2">
-          <span className="code">@gray2</span>
-        </span>
+        <span className="color-block gray2"></span>
+        <span className="code code-left">@gray2</span>
+        <span className="code code-hex">#626668</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray3">
-          <span className="code">@gray3</span>
-        </span>
+        <span className="color-block gray3"></span>
+        <span className="code code-left">@gray3</span>
+        <span className="code code-hex">#818487</span>
       </div>
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block gray4">
-          <span className="code">@gray4</span>
-        </span>
+        <span className="color-block gray4"></span>
+        <span className="code code-left">@gray4</span>
+        <span className="code code-hex">#9fa1a2</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray5">
-          <span className="code">@gray5</span>
-        </span>
+        <span className="color-block gray5"></span>
+        <span className="code code-left">@gray5</span>
+        <span className="code code-hex">#babdbe</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray6">
-          <span className="code">@gray6</span>
-        </span>
+        <span className="color-block gray6"></span>
+        <span className="code code-left">@gray6</span>
+        <span className="code code-hex">#d7dbdb</span>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray7">
-          <span className="code">@gray7</span>
-        </span>
+        <span className="color-block gray7"></span>
+        <span className="code code-left">@gray7</span>
+        <span className="code code-hex">#e8e9e9</span>
       </div>
     </div>
     <div className="row u-mb-5">
       <div className="columns small-3">    
-        <span className="color-block gray8">
-          <span className="code">@gray8</span>
-        </span>
-      </div>
-      <div className="columns small-3">
-        <span className="color-block mongodbslate">
-          <span className="code">@mongodbslate</span>
-        </span>
+        <span className="color-block gray8"></span>
+        <span className="code code-left">@gray8</span>
+        <span className="code code-hex">#f5f6f7</span>
       </div>
     </div>
 
