88f4f4bd1e1059dcbae77cb2f5cc0a3806b1d87e
Joy Sampoonachot <joy.sampoonachot@gmail.com>
2017-09-21 14:41:49 -0400

    bemified color-chip

--- client/components/ui-design-system/base-styles/colors/index.jsx
+++ client/components/ui-design-system/base-styles/colors/index.jsx
@@ -19,41 +19,55 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block green0"></span>
-        <span className="code code-left">@green0</span>
-        <span className="code code-hex">#365221</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green0 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green0</span>
+          <p className="color-chip-hex">HEX #365221</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block green1"></span>
-        <span className="code code-left">@green1</span>
-        <span className="code code-hex">#507b32</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green1 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green1</span>
+          <p className="color-chip-hex">HEX #507b32</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block green2"></span>
-        <span className="code code-left">@green2</span>
-        <span className="code code-hex">#6ba442</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green2 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green2</span>
+          <p className="color-chip-hex">HEX #6ba442</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block green3"></span>
-        <span className="code code-left">@green3</span>
-        <span className="code code-hex">#89b668</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green3 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green3</span>
+          <p className="color-chip-hex">HEX #89b668</p>
+        </div>
       </div>
     </div>
     <div className="row u-mb-5">
       <div className="columns small-3">
-        <span className="color-block green4"></span>
-        <span className="code code-left">@green4</span>
-        <span className="code code-hex">#a6c88e</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green4 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green4</span>
+          <p className="color-chip-hex">HEX #a6c88e</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block green5"></span>
-        <span className="code code-left">@green5</span>
-        <span className="code code-hex">#c4dbb3</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green5 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green5</span>
+          <p className="color-chip-hex">HEX #c4dbb3</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block green8"></span>
-        <span className="code code-left">@green8</span>
-        <span className="code code-hex">#f0f7ec</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch green8 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@green8</span>
+          <p className="color-chip-hex">HEX #f0f7ec</p>
+        </div>
       </div>
     </div>
 
@@ -64,53 +78,71 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block gray0"></span>
-        <span className="code code-left">@gray0</span>
-        <span className="code code-hex">#303434</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray0 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray0</span>
+          <p className="color-chip-hex">HEX #303434</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray1"></span>
-        <span className="code code-left">@gray1</span>
-        <span className="code code-hex">#464c4f</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray1 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray1</span>
+          <p className="color-chip-hex">HEX #464c4f</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray2"></span>
-        <span className="code code-left">@gray2</span>
-        <span className="code code-hex">#626668</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray2 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray2</span>
+          <p className="color-chip-hex">HEX #626668</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray3"></span>
-        <span className="code code-left">@gray3</span>
-        <span className="code code-hex">#818487</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray3 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray3</span>
+          <p className="color-chip-hex">HEX #818487</p>
+        </div>
       </div>
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block gray4"></span>
-        <span className="code code-left">@gray4</span>
-        <span className="code code-hex">#9fa1a2</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray4 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray4</span>
+          <p className="color-chip-hex">HEX #9fa1a2</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray5"></span>
-        <span className="code code-left">@gray5</span>
-        <span className="code code-hex">#babdbe</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray5 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray5</span>
+          <p className="color-chip-hex">HEX #babdbe</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray6"></span>
-        <span className="code code-left">@gray6</span>
-        <span className="code code-hex">#d7dbdb</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray6 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray6</span>
+          <p className="color-chip-hex">HEX #d7dbdb</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block gray7"></span>
-        <span className="code code-left">@gray7</span>
-        <span className="code code-hex">#e8e9e9</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch gray7 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray7</span>
+          <p className="color-chip-hex">HEX #e8e9e9</p>
+        </div>
       </div>
     </div>
     <div className="row u-mb-5">
-      <div className="columns small-3">    
-        <span className="color-block gray8"></span>
-        <span className="code code-left">@gray8</span>
-        <span className="code code-hex">#f5f6f7</span>
+      <div className="columns small-3">
+        <div className="color-chip">
+          <span className="color-chip-swatch gray8 u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@gray8</span>
+          <p className="color-chip-hex">HEX #f5f6f7</p>
+        </div>
       </div>
     </div>
 
@@ -121,36 +153,48 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block alert-orange"></span>
-        <span className="code code-left">@alertOrange</span>
-        <span className="code code-hex">#fbb129</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-orange u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertOrange</span>
+          <p className="color-chip-hex">HEX #fbb129</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block alert-blue"></span>
-        <span className="code code-left">@alertBlue</span>
-        <span className="code code-hex">#43b1e5</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-blue u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertBlue</span>
+          <p className="color-chip-hex">HEX #43b1e5</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block alert-red"></span>
-        <span className="code code-left">@alertRed</span>
-        <span className="code code-hex">#ef4c4c</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-red u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertRed</span>
+          <p className="color-chip-hex">HEX #ef4c4c</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block alert-green"></span>
-        <span className="code code-left">@alertGreen</span>
-        <span className="code code-hex">#69B241</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-green u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertGreen</span>
+          <p className="color-chip-hex">HEX #69B241</p>
+        </div>
       </div>
     </div>
     <div className="row u-mb-5">
       <div className="columns small-3">
-        <span className="color-block alert-blue-input-disabled"></span>
-        <span className="code code-left">@alertBlueInputDisabled</span>
-        <span className="code code-hex">#C9DBE4</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-blue-input-disabled u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertBlueInputDisabled</span>
+          <p className="color-chip-hex">HEX #C9DBE4</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block alert-blue-text-disabled"></span>
-        <span className="code code-left">@alertBlueTextDisabled</span>
-        <span className="code code-hex">#5A869C</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch alert-blue-text-disabled u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@alertBlueTextDisabled</span>
+          <p className="color-chip-hex">HEX #5A869C</p>
+        </div>
       </div>
     </div>
 
@@ -161,46 +205,62 @@ const UIColors = () => (
     </div>
     <div className="row u-mb-3">
       <div className="columns small-3">
-        <span className="color-block link-text"></span>
-        <span className="code code-left">@linkText</span>
-        <span className="code code-hex">#006cbc</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch link-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@linkText</span>
+          <p className="color-chip-hex">HEX #006cbc</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block link-text-on-dark"></span>
-        <span className="code code-left">@linkOnDark</span>
-        <span className="code code-hex">#2898dd</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch link-text-on-dark u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@linkTextOnDark</span>
+          <p className="color-chip-hex">HEX #2898dd</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block warning-text"></span>
-        <span className="code code-left">@warningText</span>
-        <span className="code code-hex">#fbb129</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch warning-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@warningText</span>
+          <p className="color-chip-hex">HEX #fbb129</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block error-text"></span>
-        <span className="code code-left">@errorText</span>
-        <span className="code code-hex">#ef4c4c</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch error-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@errorText</span>
+          <p className="color-chip-hex">HEX #ef4c4c</p>
+        </div>
       </div>
     </div>
     <div className="row u-mb-5">
       <div className="columns small-3">
-        <span className="color-block success-text"></span>
-        <span className="code code-left">@successText</span>
-        <span className="code code-hex">#69B241</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch success-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@successText</span>
+          <p className="color-chip-hex">HEX #69B241</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block muted-text"></span>
-        <span className="code code-left">@mutedText</span>
-        <span className="code code-hex">#9fa1a2</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch muted-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@mutedText</span>
+          <p className="color-chip-hex">HEX #9fa1a2</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block very-muted-text"></span>
-        <span className="code code-left">@veryMutedText</span>
-        <span className="code code-hex">#babdbe</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch very-muted-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@veryMutedText</span>
+          <p className="color-chip-hex">HEX #babdbe</p>
+        </div>
       </div>
       <div className="columns small-3">
-        <span className="color-block sub-nav-text"></span>
-        <span className="code code-left">@subNavText</span>
-        <span className="code code-hex">#6f6763</span>
+        <div className="color-chip">
+          <span className="color-chip-swatch sub-nav-text u-mb-2"></span>
+          <span className="color-chip-code code u-mb-1">@subNavText</span>
+          <p className="color-chip-hex">HEX #6f6763</p>
+        </div>
       </div>
     </div>
 
