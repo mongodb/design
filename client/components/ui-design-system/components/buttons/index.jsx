42a2f3c8fe6178ff2ab1f460aa9769457f8cc6f6
Marc Schäffner-Gurney <mws8726@gmail.com>
2017-10-02 21:45:00 -0400

    starting to add react-css toggle for component page

--- client/components/ui-design-system/components/buttons/index.jsx
+++ client/components/ui-design-system/components/buttons/index.jsx
@@ -3,12 +3,25 @@
 // ==================================================
 
 import React from 'react';
+import { Link } from 'react-router';
 
 const UIButtons = () => (
   <div className="wrap button-ui">
     <div className="row u-mb-3">
       <div className="columns small-12">
         <h1>Buttons</h1>
+      </div>
+    </div>
+    <div className="row u-mb-2">
+      <div className="columns small-12">
+        <ul className="tabs">
+          <li className="tabs-tab tabs-tab-is-active">
+            <Link to='/ui-design-system/components/buttons' className="tabs-tab-link">CSS</Link>
+          </li>
+          <li className="tabs-tab">
+            <Link to='/ui-design-system/components/buttons/react-buttons' className="tabs-tab-link">React</Link>
+          </li>
+        </ul>
       </div>
     </div>
     <div className="row u-mb-2">
--- client/routes.jsx
+++ client/routes.jsx
@@ -27,6 +27,7 @@ import UIQueryHistory from './components/ui-design-system/compass-components/que
 import UICode from './components/ui-design-system/components/code/index.jsx';
 import UIBanners from './components/ui-design-system/components/banners/index.jsx';
 import UIButtons from './components/ui-design-system/components/buttons/index.jsx';
+import UIButtonsReact from './components/ui-design-system/components/buttons/react-buttons.jsx';
 import UIBubbles from './components/ui-design-system/components/bubbles/index.jsx';
 import UIForms from './components/ui-design-system/components/forms/index.jsx';
 import UILayouts from './components/ui-design-system/components/layouts/index.jsx';
@@ -64,6 +65,7 @@ const routes = (
       <Route path="/ui-design-system/components/banners" component={UIBanners} />
       <Route path="/ui-design-system/components/bubbles" component={UIBubbles} />
       <Route path="/ui-design-system/components/buttons" component={UIButtons} />
+      <Route path="/ui-design-system/components/buttons/react-buttons" component={UIButtonsReact} />
       <Route path="/ui-design-system/components/forms" component={UIForms} />
       <Route path="/ui-design-system/components/layouts" component={UILayouts} />
       <Route path="/ui-design-system/components/lists" component={UILists} />
