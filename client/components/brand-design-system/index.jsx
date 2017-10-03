a73e638ecda08acf116cde16ab58457c8618bae7
Sean Oh <sean.oh.1994@gmail.com>
2017-10-03 10:25:50 -0400

    Merge conflict

--- client/components/brand-design-system/index.jsx
+++ client/components/brand-design-system/index.jsx
@@ -8,18 +8,18 @@ import '../../styling/root.less';
 
 const BrandDesignSystem = ({ children }) => (
   <div className="wrap">
-    <nav className="navigation">
-     	<ul className="navigation navigation-is-vertical navigation-is-vertical-parent">
+    <nav className="navigation-is-vertical">
+     	<ul className="navigation-is-vertical-parent">
         <li><h4>Brand Design</h4>
-          <ul className="navigation-is-vertical-parent">
-            <li className="navigation-is-vertical-child"><Link to='/brand-design-system/' className="navigation-link">Welcome</Link></li>
+          <ul className="navigation-is-vertical-child">
+            <li><Link to='/brand-design-system/' className="navigation-link">Welcome</Link></li>
           </ul>
         </li>
         <li><h4>Base Styles</h4>
-          <ul className="navigation-is-vertical-parent">
-            <li className="navigation-is-vertical-child"><Link to='/brand-design-system/logo' className="navigation-link">Logo</Link></li>
-            <li className="navigation-is-vertical-child"><Link to='/brand-design-system/typography' className="navigation-link">Typography</Link></li>
-            <li className="navigation-is-vertical-child"><Link to='/brand-design-system/colors' className="navigation-link">Colors</Link></li>
+          <ul className="navigation-is-vertical-child">
+            <li><Link to='/brand-design-system/logo' className="navigation-link">Logo</Link></li>
+            <li><Link to='/brand-design-system/typography' className="navigation-link">Typography</Link></li>
+            <li><Link to='/brand-design-system/colors' className="navigation-link">Colors</Link></li>
           </ul>
         </li>
     	</ul>
