9f68fa4d339351b0b6adcd54d2284ee8a46b9439
Marc Schäffner-Gurney <mws8726@gmail.com>
2017-10-05 16:13:58 -0400

    added Button.js from Cloud

--- client/components/ui-design-system/components/buttons/react-buttons.jsx
+++ client/components/ui-design-system/components/buttons/react-buttons.jsx
@@ -4,6 +4,8 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import Code from '../../subcomponents/code';
+import Button from '../../react-components/Button.js';
 
 const UIButtonsReact = () => (
   <div className="wrap button-ui">
@@ -26,28 +28,27 @@ const UIButtonsReact = () => (
     </div>
     <div className="row u-mb-2">
       <div className="columns small-12">
-        <h2>[React Version]</h2>
+        <h2>Button Component</h2>
         <p>Our button styles comes in a variety of flavors including default, primary, destructive and disabled.</p>
       </div>
     </div>
     <div className="row u-mb-2">
       <div className="columns small-12">
-        <button className="button u-mr-2">Default Button</button>
-        <button className="button button-is-primary u-mr-2">Primary Button</button>
-        <button className="button button-is-danger u-mr-2">Destructive Button</button>
-        <button className="button" disabled>Disabled Button</button>
+        <Button
+          label="Default Button"
+          className="button-is-default"
+        />
       </div>
     </div>
     <div className="row u-mb-3">
       <div className="columns small-12">
-        <pre>
-          <code className="language-html hljs xml" data-lang="html">
-            <div>{ '<button class="button">Default Button</button>' }</div>
-            <div>{ '<button class="button button-is-primary">Primary Button</button>' }</div>
-            <div>{ '<button class="button button-is-danger">Destructive Button</button>' }</div>
-            <div>{ '<button class="button" disabled>Disabled Button</button>' }</div>
-          </code>
-        </pre>
+        <Code
+        language='language-jsx'
+        text={`<Button
+  label="Default Button"
+  className="button-is-default"
+/>`}>
+      </Code>
       </div>
     </div>
   </div>
