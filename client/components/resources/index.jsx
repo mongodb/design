6fb0630969c2fc3222ec11780729ac3e55ca4af7
Marc Schäffner-Gurney <mws8726@gmail.com>
2017-10-25 14:19:02 -0400

    updating content

--- client/components/resources/index.jsx
+++ client/components/resources/index.jsx
@@ -7,8 +7,23 @@ import '../../styling/root.less';
 
 const Resources = () => (
   <div className="wrap">
-    <h1 className="heading">Resources</h1>
-    <div>🙈</div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <h1 className="heading">Resources</h1>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <h2 className="heading">Brand Guide</h2>
+        <p>Looking for the brand guide? Visit the <a href="https://mongodb.frontify.com" target="_blank">MongoDB Brand Portal</a> for logos, icons, color palettes, illustration guidelines, and more.</p>
+      </div>
+    </div>
+    <div className="row u-mb-3">
+      <div className="columns small-12">
+        <h2 className="heading">Sketch Templates</h2>
+        <p>[put download links here]</p>
+      </div>
+    </div>
   </div>
 );
 
