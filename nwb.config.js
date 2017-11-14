3ef554c74179a6b80095d749a8ef250b36cfe784
Marc Schäffner-Gurney <mws8726@gmail.com>
2017-11-14 15:56:30 -0500

    nwb config

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = {
   type: 'react-app',
   webpack: {
@@ -14,6 +16,16 @@ module.exports = {
     publicPath: '',
     html: {
       template: 'client/index.html'
+    },
+    aliases: {
+      // Enable use of 'img/file.png' paths in JavaScript and
+      // "~images/file.png" paths in stylesheets to require an image from
+      // src/images from anywhere in the the app.
+      'img': path.resolve('design/client/assets'),
+      // Enable use of require('src/path/to/module.js') for top-down imports
+      // from anywhere in the app, to promote writing location-independent
+      // code by avoiding ../ directory traversal.
+      // 'src': path.resolve('src')
     },
   },
 
