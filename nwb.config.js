3983ae18815c071953caf835300c4769f63bffb4
Marc Schäffner-Gurney <mws8726@gmail.com>
2017-11-14 16:35:40 -0500

    nwb config

--- nwb.config.js
+++ nwb.config.js
@@ -21,7 +21,7 @@ module.exports = {
       // Enable use of 'img/file.png' paths in JavaScript and
       // "~images/file.png" paths in stylesheets to require an image from
       // src/images from anywhere in the the app.
-      'img': path.resolve('design/client/assets'),
+      'assets': path.resolve('client/assets'),
       // Enable use of require('src/path/to/module.js') for top-down imports
       // from anywhere in the app, to promote writing location-independent
       // code by avoiding ../ directory traversal.
