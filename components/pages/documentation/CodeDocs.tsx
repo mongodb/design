40012465bdefb4bfbee6821c779fe7de97374046
Adam Thompson <adam.thompson@mongodb.com>
2022-08-24 12:02:50 -0400

    Break out MarkdownPropTable

--- components/pages/documentation/CodeDocs.tsx
+++ components/pages/documentation/CodeDocs.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { unified } from 'unified';
-import markdown from 'remark-parse';
-import remarkGfm from 'remark-gfm';
 import { BaseLayoutProps } from 'utils/types';
-import { GridContainer, GridItem } from 'components/Grid';
-import PropTable, { ReadmeMarkdown } from './PropTable';
-import TypeDefinition from './TypeDefinition';
 import { InstallInstructions } from './InstallInstructions';
 import { CodeExample } from './CodeExample';
+import { MarkdownPropTable } from './MarkdownPropTable/MarkdownPropTable';
 function CodeDocs({
   componentName,
   componentKebabCaseName,
   readme,
   changelog,
 }: BaseLayoutProps) {
-  const markdownAst = unified()
-    .use(markdown)
-    .use(remarkGfm)
-    .parse(readme) as unknown as ReadmeMarkdown;
-
   return (
     <>
       <InstallInstructions
@@ -26,12 +16,7 @@ function CodeDocs({
         changelog={changelog}
       />
       <CodeExample componentName={componentName} readme={readme} />
-      <GridContainer align="flex-start" justify="flex-start">
-        <GridItem sm={12} md={12} xl={12}>
-          <PropTable markdownAst={markdownAst} component={componentName} />
-          <TypeDefinition markdownAst={markdownAst} readme={readme} />
-        </GridItem>
-      </GridContainer>
+      <MarkdownPropTable componentName={componentName} readme={readme} />
     </>
   );
 }
---
+++ components/pages/documentation/MarkdownPropTable/MarkdownPropTable.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { unified } from 'unified';
+import markdown from 'remark-parse';
+import remarkGfm from 'remark-gfm';
+import { GridContainer, GridItem } from 'components/Grid';
+import PropTable, { ReadmeMarkdown } from './PropTable';
+import TypeDefinition from './TypeDefinition';
+
+export const MarkdownPropTable = ({ componentName, readme }) => {
+  const markdownAst = unified()
+    .use(markdown)
+    .use(remarkGfm)
+    .parse(readme) as unknown as ReadmeMarkdown;
+
+  return (
+    <GridContainer align="flex-start" justify="flex-start">
+      <GridItem sm={12} md={12} xl={12}>
+        <PropTable markdownAst={markdownAst} component={componentName} />
+        <TypeDefinition markdownAst={markdownAst} readme={readme} />
+      </GridItem>
+    </GridContainer>
+  );
+};
