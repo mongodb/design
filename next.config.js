441981e45e2edb73b08cb208c1206c914214a8eb
spark33 <seanpark1107@gmail.com>
2022-09-20 16:46:58 -0400

    removing commented code from next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// import rehypeSlug from 'rehype-slug';
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-// import nextMdx from '@next/mdx';
-
-// const withMDX = nextMdx({
-//   extension: /\.mdx?$/,
-//   options: {
-//     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
-//     providerImportSource: '@mdx-js/react',
-//   },
-// });
-
 module.exports = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx', 'ts'],
   trailingSlash: true,
@@ -17,5 +5,3 @@ module.exports = {
     domains: ['images.ctfassets.net'],
   }
 };
-
-// export default nextConfig; 
