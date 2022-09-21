15d12fbaa6fdd31cab7a675db85a99bfc2ebebf3
spark33 <seanpark1107@gmail.com>
2022-09-21 12:34:03 -0400

    updating update-packages job to handle empty arrays

--- scripts/update-packages.js
+++ scripts/update-packages.js
@@ -1,1 +1,3 @@
-const updatesArray = JSON.parse(process.argv[2]);
+const updatesArray = process.argv[2] ? JSON.parse(process.argv[2]) : [];
+
+console.log(`${updatesArray.length} packages were updated.`);
