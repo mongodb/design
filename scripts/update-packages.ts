bf5a34db73930fd116ce39832f1a9546b5b5f282
spark33 <seanpark1107@gmail.com>
2022-09-21 13:11:16 -0400

    update update-packages script

---
+++ scripts/update-packages.ts
@@ -0,0 +1,41 @@
+import { spawn } from 'child_process';
+import chalk from 'chalk';
+
+interface ComponentUpdateObject {
+  name: string;
+  version: string;
+}
+
+const isComponentUpdateObject = (obj: any): obj is ComponentUpdateObject =>
+  typeof obj === 'object' &&
+  Object.prototype.hasOwnProperty.call(obj, 'name') &&
+  Object.prototype.hasOwnProperty.call(obj, 'version');
+const isValidUpdatesArray = (arr: any): arr is Array<ComponentUpdateObject> =>
+  Array.isArray(arr) && arr.every(isComponentUpdateObject);
+
+function exists(arg?: string | Array<any>) {
+  return arg !== undefined && arg.length > 0;
+}
+
+try {
+  const updatesArray = process.argv[2] ? JSON.parse(process.argv[2]) : [];
+
+  if (exists(updatesArray) && isValidUpdatesArray(updatesArray)) {
+    updatesArray.forEach(({ name, version }: ComponentUpdateObject) => {
+      let cmd = spawn('yarn', ['upgrade', name])
+      cmd.on('close', code => {
+        console.log(
+          code === 0
+            ? chalk.green.bold(
+              `\n✅ Upgraded ${name} to ${version}.`,
+            )
+            : chalk.red.bold(`\nExit code ${code}\n`),
+        );
+      })
+    })
+  }
+} catch (error) {
+  console.error(error)
+}
+
+export { }
