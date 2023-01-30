945a47bf083203420ea1bec6c87255eff75450e5
Adam Thompson <adam.thompson@mongodb.com>
2023-01-30 17:21:03 -0500

    Sorts TSDoc props

--- components/pages/documentation/CodeDocs.tsx
+++ components/pages/documentation/CodeDocs.tsx
@@ -1,3 +1,5 @@
+import { kebabCase } from 'lodash';
+import { CustomComponentDoc } from 'utils/tsdoc.utils';
 import { BaseLayoutProps } from 'utils/types';
 
 import { Subtitle } from '@leafygreen-ui/typography';
@@ -8,8 +10,23 @@ import { InstallInstructions } from './InstallInstructions';
 function CodeDocs({
   componentKebabCaseName,
   changelog,
-  tsDoc: tsDocArray,
+  tsDoc,
 }: BaseLayoutProps) {
+  const tsDocArray = tsDoc?.sort(
+    (
+      { displayName: a }: CustomComponentDoc,
+      { displayName: b }: CustomComponentDoc,
+    ) => {
+      // Display name that match the package name are sorted first
+      if (kebabCase(a) === componentKebabCaseName) return -1;
+      else if (kebabCase(b) === componentKebabCaseName) return 1;
+      // Next are display names that include the package name
+      else if (kebabCase(a).includes(componentKebabCaseName)) return -1;
+      else if (kebabCase(b).includes(componentKebabCaseName)) return 1;
+      // The rest of the package components are sorted alphabetically
+      else return a.localeCompare(b);
+    },
+  );
   return (
     <>
       <InstallInstructions
