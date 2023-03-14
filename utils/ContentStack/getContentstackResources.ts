c5c004903c9e0afed76da607a0aa1318193fd0a2
spark33 <seanpark1107@gmail.com>
2023-03-14 17:24:48 -0400

    make DEPLOYMENT NEXT_PUBLIC_

--- utils/ContentStack/getContentstackResources.ts
+++ utils/ContentStack/getContentstackResources.ts
@@ -13,7 +13,9 @@ const Stack = Contentstack.Stack({
   api_key: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
   delivery_token: process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
   environment:
-    (process.env.DEPLOYMENT as string) === 'production' ? 'main' : 'staging',
+    (process.env.NEXT_PUBLIC_ENVIRONMENT as string) === 'production'
+      ? 'main'
+      : 'staging',
 });
 
 interface QueryOptions {
