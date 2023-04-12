1c35a7f8bb4247b98a4ab5517fe30516ca19195e
Adam Thompson <adam.thompson@mongodb.com>
2023-04-12 16:16:59 -0400

    forward ref in nextLink wrapper

--- components/NextLinkWrapper.tsx
+++ components/NextLinkWrapper.tsx
@@ -1,7 +1,13 @@
-import NextLink from 'next/link';
+import { forwardRef, PropsWithChildren } from 'react';
+import NextLink, { LinkProps } from 'next/link';
 
-export const NextLinkWrapper = ({ href, children, ...props }) => (
-  <NextLink href={href}>
+export const NextLinkWrapper = forwardRef<
+  HTMLAnchorElement,
+  PropsWithChildren<LinkProps>
+>(({ href, children, ...props }, ref) => (
+  <NextLink href={href} ref={ref}>
     <a {...props}>{children}</a>
   </NextLink>
-);
+));
+
+NextLinkWrapper.displayName = 'NextLinkWrapper';
