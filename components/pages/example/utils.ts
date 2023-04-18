eb269b410598303b90792ae2e46ce9d55bd648f6
spark33 <seanpark1107@gmail.com>
2023-04-18 13:26:16 -0400

    consume new displayedPlatforms prop from StoryMeta

--- components/pages/example/utils.ts
+++ components/pages/example/utils.ts
@@ -157,7 +157,7 @@ function getSBInputTypeFilterFn({
       ['none', false].includes(input.control) ||
       ['none', false].includes(localInput?.control);
 
-    const isSBOnly: boolean = meta?.argTypes?.[input.name]?.storybookOnly;
+    const isSBOnly: boolean = meta?.argTypes?.[input.name]?.displayedPlatforms === 'storybookOnly';
     const isExcludedByMeta: boolean =
       meta?.parameters?.controls?.exclude?.includes(input.name);
 
