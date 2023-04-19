7678517e26d70d4deb39756068b25147e79ad2b7
Adam Thompson <adam.thompson@mongodb.com>
2023-04-19 13:26:27 -0400

    Update LiveExampleLoading.tsx

--- components/pages/example/LiveExampleStateComponents/LiveExampleLoading.tsx
+++ components/pages/example/LiveExampleStateComponents/LiveExampleLoading.tsx
@@ -1,28 +1,9 @@
-import { css, keyframes } from '@leafygreen-ui/emotion';
-import Icon from '@leafygreen-ui/icon';
-import { H2 } from '@leafygreen-ui/typography';
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg)
-  }
-  to {
-    transform: rotate(360deg)
-  }
-`;
+import { VisuallyHidden } from '@leafygreen-ui/a11y';
 
 export function LiveExampleLoading() {
   return (
-    <H2>
-      <Icon
-        glyph="Refresh"
-        className={css`
-          height: 0.75em;
-          width: 0.75em;
-          animation: ${rotate} 1s linear infinite;
-        `}
-      />{' '}
-      Loading live example...
-    </H2>
+    <div>
+      <VisuallyHidden>Loading live example...</VisuallyHidden>
+    </div>
   );
 }
