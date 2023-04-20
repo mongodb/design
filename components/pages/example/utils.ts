0b8745a624691a650d13549cd23df4e2d3ac95ec
spark33 <seanpark1107@gmail.com>
2023-04-19 20:26:22 -0400

    Revert "contentstack automation webhook handler wip"
    This reverts commit 4f5ecbda4f396490c8973886f93564cb79e350a5.

--- components/pages/example/utils.ts
+++ components/pages/example/utils.ts
@@ -157,8 +157,7 @@ function getSBInputTypeFilterFn({
       ['none', false].includes(input.control) ||
       ['none', false].includes(localInput?.control);
 
-    const isSBOnly: boolean =
-      meta?.argTypes?.[input.name]?.displayedPlatforms === 'storybookOnly';
+    const isSBOnly: boolean = meta?.argTypes?.[input.name]?.displayedPlatforms === 'storybookOnly';
     const isExcludedByMeta: boolean =
       meta?.parameters?.controls?.exclude?.includes(input.name);
 
