adc09597068fd6fd7d9b22979921af96f54a6975
Adam Thompson <adam.thompson@mongodb.com>
2023-04-21 14:58:27 -0400

    add line break in error page

--- components/pages/example/LiveExampleStateComponents/LiveExampleError.tsx
+++ components/pages/example/LiveExampleStateComponents/LiveExampleError.tsx
@@ -4,6 +4,7 @@ export function LiveExampleError({ message }: { message?: string }) {
   return (
     <>
       <H2>⚠️ Error loading live example</H2>
+      <br />
       <InlineCode>{message}</InlineCode>
     </>
   );
