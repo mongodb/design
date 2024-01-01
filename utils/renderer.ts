5891d2083a322267cde157552483e411c78f1d60
liao-frank <liaofranku@gmail.com>
2023-12-31 22:45:36 -0800

    fix: extract default critical styles to prevent FOUC
    use the default emotion cache to extract default critical styles during SSR

--- utils/renderer.ts
+++ utils/renderer.ts
@@ -1,4 +1,10 @@
-import { extractCritical } from '@leafygreen-ui/emotion';
+import createEmotionServer from '@emotion/server/create-instance';
+
+import { extractCritical as extractLeafygreenCritical } from '@leafygreen-ui/emotion';
+
+import { cache } from '@emotion/css';
+
+const { extractCritical } = createEmotionServer(cache);
 
 export const renderStatic = async (callback: () => string) => {
   const html = callback();
@@ -7,9 +13,11 @@ export const renderStatic = async (callback: () => string) => {
     throw new Error('did you forget to return html from renderToString?');
   }
 
+  const { ids, css } = extractCritical(html);
   // Extracts critical styles from the custom LeafyGreen UI Emotion Server
   // https://github.com/mongodb/leafygreen-ui/tree/main/packages/emotion#server-side-rendering
-  const { ids, css } = extractCritical(html);
+  const { ids: leafygreenIds, css: leafygreenCss } =
+    extractLeafygreenCritical(html);
 
-  return { html, ids, css };
+  return { html, ids: [...ids, ...leafygreenIds], css: css + leafygreenCss };
 };
