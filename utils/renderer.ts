92bf8b634df0ce2085508e962a652ac872496d91
liao-frank <liaofranku@gmail.com>
2024-01-01 18:58:24 -0800

    fix: fix incorrect key on critical styles tag + refactor

--- utils/renderer.ts
+++ utils/renderer.ts
@@ -1,23 +1,41 @@
+import type { EmotionCache } from '@emotion/css';
+import type { EmotionCritical } from '@emotion/server/create-instance';
 import createEmotionServer from '@emotion/server/create-instance';
 
-import { extractCritical as extractLeafygreenCritical } from '@leafygreen-ui/emotion';
+import {
+  cache as lgCache,
+  extractCritical as extractLgCritical,
+} from '@leafygreen-ui/emotion';
 
 import { cache } from '@emotion/css';
 
 const { extractCritical } = createEmotionServer(cache);
 
-export const renderStatic = async (callback: () => string) => {
+export const renderStatic = (callback: () => string) => {
   const html = callback();
 
   if (html === undefined) {
     throw new Error('did you forget to return html from renderToString?');
   }
 
-  const { ids, css } = extractCritical(html);
-  // Extracts critical styles from the custom LeafyGreen UI Emotion Server
-  // https://github.com/mongodb/leafygreen-ui/tree/main/packages/emotion#server-side-rendering
-  const { ids: leafygreenIds, css: leafygreenCss } =
-    extractLeafygreenCritical(html);
+  return {
+    html,
+    styles: [
+      createStylesObjFromCritical(cache, extractCritical(html)),
+      // Extract critical styles from the custom LeafyGreen UI Emotion Server
+      // https://github.com/mongodb/leafygreen-ui/tree/main/packages/emotion#server-side-rendering
+      createStylesObjFromCritical(lgCache, extractLgCritical(html)),
+    ],
+  };
+};
 
-  return { html, ids: [...ids, ...leafygreenIds], css: css + leafygreenCss };
+const createStylesObjFromCritical = (
+  cache: EmotionCache,
+  critical: EmotionCritical,
+) => {
+  return {
+    key: cache.key,
+    ids: critical.ids,
+    css: critical.css,
+  };
 };
