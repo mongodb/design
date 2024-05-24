8ec99b9e6eaf89b40311e48fc983903136426eca
Adam Thompson <adam.thompson@mongodb.com>
2024-05-24 10:18:43 -0400

    rm redundant error boundary

--- src/app/component/[component]/design-docs/client.tsx
+++ src/app/component/[component]/design-docs/client.tsx
@@ -2,7 +2,6 @@
 
 import { ContentstackRichText } from '@/components/content-stack';
 import { CSNode } from '@/components/content-stack/types';
-import { DocsErrorComponent } from './error';
 import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 
 interface DesignDocsContentProps {
@@ -10,9 +9,5 @@ interface DesignDocsContentProps {
 }
 
 export const DesignDocsContent = ({ content }: DesignDocsContentProps) => {
-  return (
-    <ErrorBoundary errorComponent={DocsErrorComponent}>
-      <ContentstackRichText content={content} />
-    </ErrorBoundary>
-  );
+  return <ContentstackRichText content={content} />;
 };
