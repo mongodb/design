7ac0711d4466559b5e86ffefd43da8f6f809e09d
brooke <brooke.yalof@mongodb.com>
2024-07-25 15:56:11 -0400

    TEST

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth";
-import Okta from "@auth/core/providers/okta";
+import NextAuth from 'next-auth';
+import Okta from '@auth/core/providers/okta';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Okta({
-      clientId: process.env.OKTA_CLIENT_ID,
-      clientSecret: process.env.OKTA_CLIENT_SECRET,
-      issuer: process.env.OKTA_ISSUER,
+      clientId: process.env.secrets.OKTA_CLIENT_ID,
+      clientSecret: process.env.secrets.OKTA_CLIENT_SECRET,
+      issuer: process.secrets.env.OKTA_ISSUER,
     }),
   ],
 });
