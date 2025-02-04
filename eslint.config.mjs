4e9b4bd0578ce0e95ae61bf2d247965e7df9d1c2
brooke <brooke.yalof@mongodb.com>
2025-02-04 16:46:01 -0500

    update linter too

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const patchedConfig = fixupConfigRules([...compat.extends("next/core-web-vitals")]);
+
+const config = [
+  ...patchedConfig,
+  // Add more flat configs here
+  { ignores: [".next/*"] },
+];
+
+export default config;
