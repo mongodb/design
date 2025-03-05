aef3bbe8cc2d0386fc6727cabaf5a5da3f8be382
Shaneeza <shaneeza.ali@mongodb.com>
2025-03-05 16:46:39 -0500

    remove unused imports

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Button from '@leafygreen-ui/button';
 import { BasicEmptyState } from '@leafygreen-ui/empty-state';
@@ -8,7 +7,7 @@ import { BasicEmptyState } from '@leafygreen-ui/empty-state';
 import ArrowLeftIcon from '@leafygreen-ui/icon/dist/ArrowLeft';
 // @ts-expect-error
 import LogInIcon from '@leafygreen-ui/icon/dist/LogIn';
-import { getSession, login, Session } from '@/auth';
+import { login } from '@/auth';
 import { ComingSoon, Security } from '@/components/glyphs';
 import { useSession } from '@/hooks';
 
