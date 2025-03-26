9ad3921d9a1c12fd60e1e1d1071cdc005eafc14b
Shaneeza <shaneeza.ali@mongodb.com>
2025-03-26 10:50:15 -0400

    comments

--- src/utils/mappedTitles.ts
+++ src/utils/mappedTitles.ts
@@ -2,6 +2,9 @@ import { Component, Pattern, Foundation } from '.';
 
 export type AllComponents = Component | Pattern | Foundation;
 
+/**
+ * There are instances where the title of a component in different in contentStack than the package name. This returns the correct package name for the component.
+ */
 export const mappedTitles: Partial<Record<AllComponents, string>> = {
   [Pattern.CloudNavLayout]: 'cloud-nav',
 };
