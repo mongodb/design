518db183c36d60f4a53ea8b4fcdae3d37ef800db
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-05-12 19:08:57 -0400

    chatbot popover

--- src/components/chatbot/index.tsx
+++ src/components/chatbot/index.tsx
@@ -1,20 +1,66 @@
+import Button, { Variant } from '@leafygreen-ui/button';
+import Popover from '@leafygreen-ui/popover';
 import { css } from '@leafygreen-ui/emotion';
+import Icon from '@leafygreen-ui/icon';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
-import Chatbot, { ChatWindow } from 'mongodb-chatbot-ui';
+import Chatbot, {
+  ChatWindow,
+  FloatingActionButtonTrigger,
+} from 'mongodb-chatbot-ui';
+import { useRef, useState } from 'react';
+import Card from '@leafygreen-ui/card';
+import { spacing } from '@leafygreen-ui/tokens';
 
 export const ChatbotComponent = () => {
-  const { darkMode } = useDarkMode();
   const endpoint = process.env.NEXT_PUBLIC_CHATBOT_ENDPOINT;
+  const { darkMode } = useDarkMode();
+  const fabRef = useRef<HTMLButtonElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleFABClick = () => {
+    setIsOpen(prev => !prev);
+  };
 
   return endpoint ? (
     <Chatbot darkMode={darkMode} serverBaseUrl={endpoint}>
-      <ChatWindow
+      <Button
+        ref={fabRef}
+        onClick={handleFABClick}
+        variant={Variant.BaseGreen}
+        size="large"
+        leftGlyph={<Icon glyph="Sparkle" size="large" />}
         className={css`
-          height: 100%;
+          position: fixed;
+          bottom: 24px;
+          right: 24px;
         `}
-        initialMessageText="Welcome to LeafyGreen AI. What can I help you with?"
-        // initialMessageSuggestedPrompts={suggestedPrompts}
-      />
+      >
+        {/* <span>Chat with LeafyGreen AI</span> */}
+      </Button>
+
+      <Popover
+        active={isOpen}
+        refEl={fabRef}
+        spacing={spacing[200]}
+        align={'bottom'}
+        justify={'end'}
+        className={css`
+          min-width: 33vw;
+          max-width: 50vw;
+          /* max-height: 50vh; */
+        `}
+      >
+        <ChatWindow
+          className={css`
+            height: 100%;
+            width: 100%;
+          `}
+          windowTitle="LeafyGreen AI"
+
+          // initialMessageText="Welcome to LeafyGreen AI. What can I help you with?"
+          // initialMessageSuggestedPrompts={suggestedPrompts}
+        />
+      </Popover>
     </Chatbot>
   ) : null;
 };
