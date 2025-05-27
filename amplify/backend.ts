5be245541576adceca1f2b317857974b99a0ba3c
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-05-27 18:09:20 -0400

    update amplify config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
 import { chatbot } from './functions/chatbot/resource';
 
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
 defineBackend({
   chatbot,
+  auth,
+  data,
 });
