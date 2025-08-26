e0a4938b29c87ef1e99be1fb440562be16590998
Shaneeza <shaneeza.ali@mongodb.com>
2025-08-26 09:51:22 -0400

    Refactor: Move UserMenu to a hidden Layout component and create InternalPage; comment out unused components

---
+++ src/app/internal/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InternalPage() {
+  return (
+    <div>
+      <h1>Internal Page</h1>
+      <p>This is the internal page content.</p>
+    </div>
+  );
+}
